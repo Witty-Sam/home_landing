73f7ee34599832a6c4a2729eb162c43f0d5e5cf7
Samuel <samuel.oladunjoye@veendhq.com>
2025-04-14 17:01:18 +0100

    wad

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -3,10 +3,14 @@ import React from 'react'
 const Features = () => {
   return (
     <div className="flex justify-center items-center h-screen mt-[1000px]">
+      <h2 className="text-[15px] font-thin tracking-widest mb-2 uppercase">Features</h2>
+  <h1 className="text-[28px] md:text-3xl font-bold mb-10">
+  Powerful Features to Supercharge Your <br/>Lending and BNPL Operations
+  </h1>
   <img src="./images/Features.svg" alt="Features" />
 </div>
 
   )
 }
 
-export default Features
+export default Features;
